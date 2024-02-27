928d56746ee741e9848cebbc3350496561e9f2f0
Alex <moleksii@gmail.com>
2024-02-27 17:51:21 +0200

    Add throttling

--- app/settings.py
+++ app/settings.py
@@ -134,6 +134,14 @@ AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    }
 }
 
 SPECTACULAR_SETTINGS = {
