78217517e79bbee0110f43cccf865273586931a3
Alex <moleksii@gmail.com>
2024-03-12 15:14:13 +0200

    Fixing function create_superuser

--- user/models.py
+++ user/models.py
@@ -28,20 +28,19 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
 
-def create_superuser(self, email, password, **extra_fields):
-    """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-    extra_fields.setdefault("is_staff", True)
-    extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
 
-    if extra_fields.get("is_staff") is not True:
-        raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
-    if extra_fields.get("is_superuser") is not True:
-        raise ValueError("Superuser must have is_superuser=True.")
-
-    return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
